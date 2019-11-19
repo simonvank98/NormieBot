0ee48fa4519887afffa18ce66b1c719d2f79652a
simonvank <nomisvank@hotmail.com>
2019-11-19 20:49:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require ('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	//console.log(message.content);
+	if (message.content.startsWith(`${prefix}greetings`)) {
+		message.channel.send("Homo's :wave:");
+	}
+});
+
+client.login(token);
