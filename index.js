0152965e4f3ccf2ccfd20e8d76af02741064d2d6
simonvank <nomisvank@hotmail.com>
2019-11-19 22:09:45 +0100

    changed procfile

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+app.listen(process.env.PORT || 5000);
+
 const Discord = require ('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+
+client.login(token);
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
 
-	
+
 	//console.log(message.content);
 	if (message.content.startsWith(`${prefix}greetings`)) {
 		message.channel.send("Homo's :wave:");
 	}
 });
-
-client.login(token);
