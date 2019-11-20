1d2a4b2cbfa1540e22ab32932ac0255ace6c66de
simonvank98 <39694777+simonvank98@users.noreply.github.com>
2019-11-20 10:59:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require ('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core');
 
 client.login(token);
 
@@ -9,10 +10,14 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-
-
-	//console.log(message.content);
 	if (message.content.startsWith(`${prefix}greetings`)) {
+		if(!message.member.voiceChannel) return message.channel.send('Je zit niet in een channel gekkie');
+		if(message.guild.me.voiceChannel) return message.channel.send('Ik ben al ergens anders Homos aan het roepen');
+		let video = await ytdl.getInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+		let connection = await message.member.voiceChannel.join();
+		let dispatcher = await connection.play(ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { filter: 'audioonly' }));
+		
+		
 		message.channel.send("Homo's :wave:");
 	}
 });
