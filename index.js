43def79eea093e006bc758626f25294c085044d6
simonvank <nomisvank@hotmail.com>
2019-11-21 22:32:42 +0100

    made it!

--- index.js
+++ index.js
@@ -2,51 +2,89 @@ const Discord = require ('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
-var servers = {};
+const queue = new Map();
 
-let args = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-
-client.login(token);
 
 client.on('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const serverQueue = queue.get(message.guild.id);
+
 	if (message.content.startsWith(`${prefix}greetings`)) {
-		function play(connection, message) {
-			var server = servers[message.guild.id];
-
-			server.dispatcher = connection.playStream(ytdl(server.queue[0], { filter: "audioonly" }));
-
-			server.queue.shift();
-
-			server.dispatcher.on("end", function () {
-				if (server.queue[0]) {
-					play(connection, message);
-				} else {
-					connection.disconnect();
-				}
-			})
-		}
-
-		if (!message.member.voiceChannel) {
-			message.channel.send('Je zit niet in een channel gekkie');
-			return;
-		}
-
-		if (!servers[message.guild.id]) servers[message.guild.id] = {
-			queue: []
-		}
-
-		var server = servers[message.guild.id];
-
-		server.queue.push(args);
-
-		if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
-			play(connection, message);
-		})
+		execute(message, serverQueue);
+		return;
 	}
 });
+
+async function execute(message, serverQueue) {
+	const args = 'https://www.youtube.com/watch?v=dWDuTZyXi78&feature=youtu.be';
+
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+
+	const songInfo = await ytdl.getInfo(args);
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+	};
+
+	if (!serverQueue) {
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} has been added to the queue!`);
+	}
+
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('Music ended!');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+
+client.login(token);
 
 
